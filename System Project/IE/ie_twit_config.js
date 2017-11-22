e2d106a1701b4af8c93a3660c5bd095442f77bd1
jm <johmarkgabrentina@gmail.com>
2017-11-22 19:34:29 +0800

    changes deleted files in the system project

--- System Project/IE/ie_twit_config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    consumer_key: 'JqZMwc1E1Ar48H7kOBTkNf0m2',
-    consumer_secret: 'lGLJtBwFZLdDTt1RjaLISffM20NhA3V4ZgXumWy2ow2BQ0TbB9',
-    access_token: '2426883066-Z2rqPGXZSNpAwNgzbBnSZO4w2B61DP9UoXA5ieJ',
-    access_token_secret: 'scKSrdN6QlMFKxQDZvqXFwvWVT6azR6pz4xrdSa5KNtWf',
-    timeout_ms: 60 * 1000, // optional HTTP request timeout to apply to all requests. 
-};
